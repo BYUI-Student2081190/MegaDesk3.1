d0a3bc55fe469a37d41bc10c5e96d2c7616017fb
byui_student1045 <sco22022@byui.edu>
2023-06-08 12:32:19 -0600

    Added Search to Index.

--- Pages/DeskQuotes/Index.cshtml.cs
+++ Pages/DeskQuotes/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MegaDesk3._0.Data;
 using MegaDesk3._0.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MegaDesk3._0.Pages.DeskQuotes
 {
@@ -21,14 +22,51 @@ namespace MegaDesk3._0.Pages.DeskQuotes
 
         public IList<DeskQuote> DeskQuote { get;set; } = default!;
 
+
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+
+
+        public SelectList? OrderDates { get; set; }
+
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime? OrderDate { get; set; }
+
+
         public async Task OnGetAsync()
         {
             if (_context.DeskQuote != null)
             {
-                DeskQuote = await _context.DeskQuote
-                .Include(d => d.DeliveryType)
-                .Include(d => d.Desk)
-                .Include(d => d.DesktopMaterial).ToListAsync();
+                // Search Through the Dates.
+                IQueryable<DateTime> dateQuery = from m in _context.DeskQuote
+                                                 orderby m.DateCreated.Date
+                                                 select m.DateCreated.Date;
+
+                // Search through DeskQuote to get customer names.
+                var orders = from m in _context.DeskQuote
+                             select m;
+
+                if (!string.IsNullOrEmpty(SearchString))
+                {
+                   orders  = orders.Where(s => s.CustomerName.Contains(SearchString));
+                }
+
+                if (OrderDate != null)
+                {
+                    orders = orders.Where(x => x.DateCreated.Date == OrderDate);
+                }
+
+                // Add the Date Search on screen.
+                OrderDates = new SelectList(await dateQuery.Distinct().ToListAsync());
+
+                // Add the searchString where applicable.
+                // This should make an new list that will be displayed on
+                // screen.
+                DeskQuote = await orders
+                                .Include(d => d.DeliveryType)
+                                .Include(d => d.Desk)
+                                .Include(d => d.DesktopMaterial).ToListAsync();
             }
         }
     }
